0e1363a9108ab7dd85ef6f59d224821e2c1fe461
jfriend08 <ys486@cornell.edu>
2016-01-11 15:44:18 -0500

    InterpolationSearch.py

---
+++ sortSearch/InterpolationSearch.py
@@ -0,0 +1,22 @@
+'''
+Interpolation search
+'''
+
+def IerpSearch(arr, target):
+  low, high = 0, len(arr)-1
+  while True:
+    if low > high:
+      return -1
+
+    m = low + (target - arr[low]) * ( (high-low)/(arr[high]-arr[low]) )
+    print low, m, high
+    if target == arr[m]:
+      return m
+    elif target > arr[m]:
+      low = m + 1
+    elif target < arr[m]:
+      high = m - 1
+
+
+arr = [1,2,50,500,101300,200000]
+print IerpSearch(arr, 101300)
