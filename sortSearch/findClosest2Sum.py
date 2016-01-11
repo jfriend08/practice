999da34ab3ff141e0aa7d5c3960f73d244da7c75
jfriend08 <ys486@cornell.edu>
2016-01-11 14:36:12 -0500

    findClosest2Sum.py

---
+++ sortSearch/findClosest2Sum.py
@@ -0,0 +1,11 @@
+'''
+Given a sorted array and a number x, find the pair in array whose sum is closest to x
+
+Examples:
+
+Input: arr[] = {10, 22, 28, 29, 30, 40}, x = 54
+Output: 22 and 30
+
+Input: arr[] = {1, 3, 4, 7, 10}, x = 15
+Output: 4 and 10
+'''
