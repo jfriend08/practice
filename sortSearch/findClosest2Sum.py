aad278e6924291587d371153657a39da467dbe23
jfriend08 <ys486@cornell.edu>
2016-01-11 15:12:16 -0500

    findClosest2Sum.py

--- sortSearch/findClosest2Sum.py
+++ sortSearch/findClosest2Sum.py
@@ -12,29 +12,53 @@ Output: 4 and 10
 
 import sys
 def findCloest2Sum(arr, target):
-  diff_pre, diff_new = sys.maxint, sys.maxint
-  idx, bestPair = 0, [None, None]
-  while diff_new <= diff_pre and idx < len(arr):
-    df_pre, df_new, localBest = sys.maxint, sys.maxint, [None, None]
 
-    for i in xrange(len(arr)-1, idx, -1):
-      df_new = abs((arr[idx]+arr[i]) - target)
-      if df_new <= df_pre:
-        localBest = [idx, i]
+  globalBest = [sys.maxint, [None, None]]
+  for i in xrange(len(arr)):
+    localBest = [sys.maxint, [None, None]]
+    for j in xrange(len(arr)-1, i, -1):
+      diff = abs((arr[i] + arr[j])-target)
+      if diff <= localBest[0]:
+        localBest = [diff, [i,j]]
       else:
         break
-      df_pre = df_new
-
-    if df_pre <= diff_pre:
-      bestPair = localBest
+    if localBest[0] <= globalBest[0]:
+      globalBest = localBest
     else:
       break
-    diff_pre = diff_new
-    diff_new = df_pre
-    idx += 1
-    print "bestPair", bestPair, diff_pre, diff_new, df_pre, df_new
+  return globalBest
+
 
   return arr[bestPair[0]], arr[bestPair[1]]
 
 arr = [10, 22, 28, 29, 30, 40]
 print findCloest2Sum(arr, 54)
+
+arr = [1, 3, 4, 7, 10]
+print findCloest2Sum(arr, 15)
+
+
+# def findCloest2Sum(arr, target):
+#   diff_pre, diff_new = sys.maxint, sys.maxint
+#   idx, bestPair = 0, [None, None]
+#   while diff_new <= diff_pre and idx < len(arr):
+#     df_pre, df_new, localBest = sys.maxint, sys.maxint, [None, None]
+
+#     for i in xrange(len(arr)-1, idx, -1):
+#       df_new = abs((arr[idx]+arr[i]) - target)
+#       if df_new <= df_pre:
+#         localBest = [idx, i]
+#       else:
+#         break
+#       df_pre = df_new
+
+#     if df_pre <= diff_pre:
+#       bestPair = localBest
+#     else:
+#       break
+#     diff_pre = diff_new
+#     diff_new = df_pre
+#     idx += 1
+#     print "bestPair", bestPair, diff_pre, diff_new, df_pre, df_new
+
+#   return arr[bestPair[0]], arr[bestPair[1]]
