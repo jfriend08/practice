266ae8f05dbacf45051e98e063b7bd836239cf85
jfriend08 <ys486@cornell.edu>
2016-01-12 16:28:07 -0500

    hasPythagoreanTriplet.py two version for finding 2 sum

--- array/hasPythagoreanTriplet.py
+++ array/hasPythagoreanTriplet.py
@@ -10,19 +10,38 @@ Input: arr[] = {10, 4, 6, 12, 5}
 Output: False
 There is no Pythagorean triplet.
 '''
+
+#based on sorted array, use two pointers
+def hasStargetSum1(target, left, right, arr):
+  while left < right:
+    ab = arr[left]+arr[right]
+    if ab == target:
+      return True
+    elif ab > target:
+      right -= 1
+    elif ab < target:
+      left += 1
+  return False
+
+#use hash
+def hasStargetSum2(target, left, right, arr):
+  need = {}
+  for i in xrange(left, right+1):
+    num = arr[i]
+    if num in need:
+      return i, need[num]
+    else:
+      need[target-num] = i
+  return False
+
+
 def hasPythagoreanTriplet(arr):
   arr.sort()
   arr = map(lambda x: x**2, arr)
   for i in xrange(len(arr)-1, 1, -1):
-    c, left, right = arr[i], 0, i-1
-    while left < right:
-      ab = arr[left]+arr[right]
-      if ab == c:
-        return True
-      elif ab > c:
-        right -= 1
-      elif ab < c:
-        left += 1
+    res = hasStargetSum2(arr[i], 0, i-1, arr)
+    if res:
+      return True
   return False
 
 
