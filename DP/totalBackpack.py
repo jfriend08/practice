64ec0bcfbfa160de6e6c80f4683588b5763ed4be
jfriend08 <ys486@cornell.edu>
2016-01-21 09:39:36 -0500

    totalBackpack.py

--- DP/totalBackpack.py
+++ DP/totalBackpack.py
@@ -1,22 +1,23 @@
 '''
-multi backpack
+total backpack
+item can put to pack for 0 to unlimited times
 '''
 
 import sys
-def multiPackUpdate(v, w, Target, arr):
+def totalPackUpdate(v, w, Target, arr):
   for weight in xrange(w, Target+1):
     arr[weight] = max(arr[weight], arr[weight-w] + v)
 
-def multiPack(V,W,Target):
+def totalPack(V,W,Target):
   arr = [0] * (Target+1)
   arr = [-sys.maxint-1] * (Target+1)
   arr[0] = 0
   for i in xrange(len(V)):
-    multiPackUpdate(V[i], W[i], Target, arr)
+    totalPackUpdate(V[i], W[i], Target, arr)
   return arr
 
 
 target = 5
 V = [10,5,7,6,3]
 W = [2,3,2,3,1]
-print multiPack(V, W, target)
+print totalPack(V, W, target)
